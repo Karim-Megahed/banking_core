e64281844f5b39ea3cb3e32e2973bdafe29927ba
Karim-Megahed <karim@amplerbikes.com>
2022-10-15 22:15:08 +0300

    added API response for customer creation

--- src/main/java/com/example/bank/customer/CustomerController.java
+++ src/main/java/com/example/bank/customer/CustomerController.java
@@ -1,6 +1,8 @@
 package com.example.bank.customer;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -12,7 +14,9 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping
-    public void createCustomer(@RequestBody @Valid CustomerRequest customerCreationRequest){
-        customerService.createCustomer(customerCreationRequest);
+    public ResponseEntity<Customer> createCustomer(@RequestBody @Valid CustomerRequest customerCreationRequest){
+        Customer customer = customerService.createCustomer(customerCreationRequest);
+
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
     }
 }
