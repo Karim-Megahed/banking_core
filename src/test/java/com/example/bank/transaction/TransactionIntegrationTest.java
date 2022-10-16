b9a9521a016bccc66232d0e22b98c432703c8f1f
Karim-Megahed <karim@amplerbikes.com>
2022-10-16 14:13:50 +0300

    TransactionIntegrationTest

--- src/test/java/com/example/bank/transaction/TransactionIntegrationTest.java
+++ src/test/java/com/example/bank/transaction/TransactionIntegrationTest.java
@@ -1,5 +1,7 @@
-package com.example.bank.account;
+package com.example.bank.transaction;
 
+import com.example.bank.account.AccountRequest;
+import com.example.bank.account.AccountResponse;
 import com.example.bank.balance.BalanceCurrency;
 import com.example.bank.customer.Customer;
 import com.example.bank.customer.CustomerRequest;
@@ -15,6 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import java.io.UnsupportedEncodingException;
 import java.util.*;
@@ -26,7 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AccountIntegrationTest {
+public class TransactionIntegrationTest {
     @Autowired
     private CustomerService customerService;
     @Autowired
@@ -35,7 +38,7 @@ public class AccountIntegrationTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void itShouldCreateAndFetchAccountSuccessfully() throws Exception {
+    void itShouldCreateAndFetchTransactionSuccessfully() throws Exception {
         CustomerRequest customerRequest = new CustomerRequest("karim", "megahed", FakeEmail());
         Customer customer = customerService.createCustomer(customerRequest);
         List<BalanceCurrency> currencies = Arrays.asList(BalanceCurrency.EUR, BalanceCurrency.GBP);
@@ -46,16 +49,22 @@ public class AccountIntegrationTest {
                 .content(Objects.requireNonNull(ObjectToJson(accountRequest))))
                 .andExpect(status().isCreated())
                 .andReturn();
-        AccountResponse accountCreationResponse = JsonToObject(accountCreationResult);
+        AccountResponse accountCreationResponse = JsonToAccountObject(accountCreationResult);
 
-        MvcResult accountFetchingResult = mockMvc.perform(get("/api/v1/accounts/" + accountCreationResponse.getAccountId())
+        TransactionRequest transactionRequest = new TransactionRequest("deposit", BalanceCurrency.EUR, TransactionDirection.IN, 10F);
+        MvcResult transactionCreationResult = mockMvc.perform(post("/api/v1/accounts/" + accountCreationResponse.getAccountId() + "/transactions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Objects.requireNonNull(ObjectToJson(transactionRequest))))
+                .andExpect(status().isCreated())
+                .andReturn();
+        TransactionResponse transactionCreationResponse = JsonToTransactionObject(transactionCreationResult);
+
+        assertThat(accountCreationResponse.getAccountId()).isEqualTo(transactionCreationResponse.getAccountId());
+
+        mockMvc.perform(get("/api/v1/accounts/" + transactionCreationResponse.getAccountId() + "/transactions")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
-        AccountResponse accountFetchingResponse = JsonToObject(accountFetchingResult);
-
-        assertThat(accountFetchingResponse).usingRecursiveComparison().isEqualTo(accountCreationResponse);
-        assertThat(accountFetchingResponse.getBalances().size()).isEqualTo(currencies.size());
     }
 
     private String FakeEmail(){
@@ -66,8 +75,12 @@ public class AccountIntegrationTest {
         return fakeValuesService.bothify("????##@gmail.com");
     }
 
-    private AccountResponse JsonToObject(MvcResult mvcResult) throws UnsupportedEncodingException, JsonProcessingException {
+    private AccountResponse JsonToAccountObject(MvcResult mvcResult) throws UnsupportedEncodingException, JsonProcessingException {
         return objectMapper.readValue(mvcResult.getResponse().getContentAsString(), AccountResponse.class);
+    }
+
+    private TransactionResponse JsonToTransactionObject(MvcResult mvcResult) throws UnsupportedEncodingException, JsonProcessingException {
+        return objectMapper.readValue(mvcResult.getResponse().getContentAsString(), TransactionResponse.class);
     }
 
     private String ObjectToJson(Object object) throws JsonProcessingException {
